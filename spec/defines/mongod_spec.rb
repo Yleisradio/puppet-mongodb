5453665c0e6016c97e34a633728cc01d9aeb9498
Jaakko Suutarla <jaakko.suutarla@yle.fi>
2013-11-19 10:19:48 +0100

    Ensure data dir is created

--- spec/defines/mongod_spec.rb
+++ spec/defines/mongod_spec.rb
@@ -13,17 +13,40 @@ describe "mongodb::mongod", :type => :define do
   it do
     should contain_file("/etc/mongod_instance_name.conf")
       .with(
-        :mode => '0644'
+        :mode => "0644"
       )
   end
 
   it do
     should contain_file("/etc/init/mongod_instance_name.conf")
       .with(
-        :mode => '0644'
+        :mode => "0644"
       )
   end
 
-  it { should contain_mongodb__logrotate("mongod_instance_name_logrotate") }
-  it { should contain_service("mongod_instance_name") }
+  it do
+    should contain_file("/opt/mongodb/instance_name/log")
+      .with(
+        :mode => "0755",
+        :owner => "mongodb",
+        :group => "mongodb"
+      )
+  end
+
+
+  it do
+    should contain_mongodb__logrotate("mongod_instance_name_logrotate")
+      .with(
+        :instance => "instance_name",
+        :logdir   => "/opt/mongodb/instance_name/log"
+      )
+  end
+
+  it do
+    should contain_service("mongod_instance_name")
+      .with(
+        :hasstatus  => true,
+        :hasrestart => true
+      )
+  end
 end
