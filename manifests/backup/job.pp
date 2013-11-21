f5c781fa7249568f52ac64f6fc87d79d4da563ac
Jussi Pöri <jussi.pori@yle.fi>
2013-11-21 16:44:36 +0200

    Removed extra whitespace., ug

--- manifests/backup/job.pp
+++ manifests/backup/job.pp
@@ -7,7 +7,6 @@ define mongodb::backup::job (
   $replica_set_backuphosts = [],
   $backupdir = ""
 ) {
-  
 
   file {
     "${::mongodb::params::homedir}/backup":
@@ -38,8 +37,8 @@ define mongodb::backup::job (
     ensure  => 'present',
     user    => $mongodb::params::run_as_user,
     command => "${::mongodb::params::homedir}/backup/run-${replica_set_name}-backup.sh",
-    hour => [ 6, 12, 18, 0 ], 
-    minute => '00', 
+    hour => [ 6, 12, 18, 0 ],
+    minute => '00',
   }
 
 
